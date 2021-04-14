5de9ef4d6140e826e7d5d8c3900625d0bd9003a9
FlyingWhale <flyingwhalestudio@icloud.com>
2021-04-14 14:40:40 +0200

    Created Sandbox scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Space");
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Debug.Log("Q");
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("D");
+        }
+    }
+}
