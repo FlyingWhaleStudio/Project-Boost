90af6ac555a8f7da3ee64a4846b66385e424e597
FlyingWhale <flyingwhalestudio@icloud.com>
2021-06-22 11:16:06 +0200

    Added Levels Loop

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -10,11 +10,26 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Friendly");
                 break;
             case "Finish":
-                Debug.Log("Finish");
+                LoadNextLevel();
                 break;
             default:
                 ReloadScene();
                 break;
+        }
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
         }
     }
 
